d1a45068c6dab5110ed2fdadc3cc80801d18f308
Patrick Glynn <pjglynn57@gmail.com>
2018-01-29 16:25:45 -0700

    working on screen recorder --careful

--- SpAy-R/ViewController.swift
+++ SpAy-R/ViewController.swift
@@ -9,14 +9,23 @@
 import UIKit
 import SceneKit
 import ARKit
+import ReplayKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, RPPreviewViewControllerDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var drawButton: UIButton!
     @IBOutlet weak var reticle: UILabel!
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var colorPicker: UISegmentedControl!
+    @IBOutlet var colorDisplay: UIView!
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var micToggle: UISwitch!
     
     var currentColor = UIColor.white
+    
+    let recorder = RPScreenRecorder.shared()
+    private var isRecording = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +41,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
 //        sceneView.scene = scene
+//       some bullshit that patrick added to try and make the screen recorder work
+        
+        recordButton.layer.cornerRadius = 32.5
     }
     
     func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
@@ -131,6 +143,94 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBAction func whiteBtn(_ sender: Any) {
         currentColor = UIColor.white
         reticle.textColor = UIColor.white
+    }
+    
+    func viewReset() {
+        micToggle.isEnabled = true
+        statusLabel.text = "Ready to Record"
+        statusLabel.textColor = UIColor.black
+        recordButton.backgroundColor = UIColor.green
+    }
+    
+    @IBAction func recordButtonTapped() {
+        if !isRecording {
+            startRecording()
+        } else {
+            stopRecording()
+        }
+    }
+    
+    
+    func startRecording() {
+        
+        
+        guard recorder.isAvailable else {
+            print("Recording is not available at this time.")
+            return
+        }
+        
+        if micToggle.isOn {
+            recorder.isMicrophoneEnabled = true
+        } else {
+            recorder.isMicrophoneEnabled = false
+        }
+        
+        recorder.startRecording{ [unowned self] (error) in
+            
+            guard error == nil else {
+                print("There was an error starting the recording.")
+                return
+            }
+            
+            print("Started Recording Successfully")
+            self.micToggle.isEnabled = false
+            self.recordButton.backgroundColor = UIColor.red
+            self.statusLabel.text = "Recording..."
+            self.statusLabel.textColor = UIColor.red
+            
+            self.isRecording = true
+            
+        }
+        
+    }
+    
+    func stopRecording() {
+        
+        recorder.stopRecording { [unowned self] (preview, error) in
+            print("Stopped recording")
+            
+            guard preview != nil else {
+                print("Preview controller is not available.")
+                return
+            }
+            
+            let alert = UIAlertController(title: "Recording Finished", message: "Would you like to edit or delete your recording?", preferredStyle: .alert)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: { (action: UIAlertAction) in
+                self.recorder.discardRecording(handler: { () -> Void in
+                    print("Recording suffessfully deleted.")
+                })
+            })
+            
+            let editAction = UIAlertAction(title: "Edit", style: .default, handler: { (action: UIAlertAction) -> Void in
+                preview?.previewControllerDelegate = self as RPPreviewViewControllerDelegate
+                self.present(preview!, animated: true, completion: nil)
+            })
+            
+            alert.addAction(editAction)
+            alert.addAction(deleteAction)
+            self.present(alert, animated: true, completion: nil)
+            
+            self.isRecording = false
+            
+            self.viewReset()
+            
+        }
+        
+    }
+    
+    func previewControllerDidFinish(_ previewController: RPPreviewViewController) {
+        dismiss(animated: true)
     }
     
 }
