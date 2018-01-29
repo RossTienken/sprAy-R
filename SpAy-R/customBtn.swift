1db400f16bffae49b438b3390dc7d9f4aaab35ac
Ross <ross.tienken99@gmail.com>
2018-01-29 14:25:57 -0700

    painting

---
+++ SpAy-R/customBtn.swift
@@ -0,0 +1,21 @@
+//
+//  customBtn.swift
+//  SpAy-R
+//
+//  Created by Ross Tienken on 1/29/18.
+//  Copyright © 2018 TheBoys. All rights reserved.
+//
+
+import UIKit
+
+class customBtn: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
